77447e8cec53ccfd55d90fc593ac029416e6f19e
Toshiaki Maki <makingx@gmail.com>
2015-08-15 12:28:46 +0900

    Genreの切り替え機能追加

--- gallery.js
+++ gallery.js
@@ -21,27 +21,48 @@ var GenresModel = {
 };
 
 var Gallery = React.createClass({
+    getInitialState: function () {
+        return {url: null};
+    },
+    setUrl: function (url) {
+        this.setState({url: url});
+    },
     render: function () {
         return (
             <div>
-                <Photos/>
-                <Genres/>
+                <Photos url={this.state.url}/>
+                <Genres onGenreChange={this.setUrl}/>
             </div>);
     }
 });
 
 var Photos = React.createClass({
     getInitialState: function () {
-        return {content: [], first: true, last: true, number: 0, size: 10, totalPages: 1};
+        return {
+            content: [],
+            first: true,
+            last: true,
+            number: 0,
+            size: 10,
+            totalPages: 1
+        };
     },
-    componentDidMount: function () {
-        PhotosModel.get()
+    loadFromServer: function (url) {
+        PhotosModel.get(url)
             .query({size: 12})
             .end(function (err, res) {
-                this.setState(res.body);
+                var state = res.body;
+                state.currentUrl = url;
+                this.setState(state);
             }.bind(this));
     },
+    componentDidMount: function () {
+        this.loadFromServer();
+    },
     render: function () {
+        if (this.props.url && this.props.url !== this.state.currentUrl) {
+            this.loadFromServer(this.props.url);
+        }
         var photos = this.state.content.map(function (x) {
             return (<Photo data={x}/>);
         });
@@ -106,9 +127,16 @@ var Genres = React.createClass({
             }.bind(this));
     },
     render: function () {
+        var links = {};
+        for (var i = 0; i < this.state._links.length; i++) {
+            var l = this.state._links[i];
+            links[l.rel] = l.href;
+        }
         var genres = this.state.content.map(function (x) {
-            return (<Genre data={x}/>);
-        });
+            return (<Genre data={x}
+                           link={links[x.genreName]}
+                           onGenreChange={this.props.onGenreChange}/>);
+        }.bind(this));
         return (
             <div className="clear">
                 <h3>Genres</h3>
@@ -120,9 +148,11 @@ var Genres = React.createClass({
 });
 
 var Genre = React.createClass({
-
+    onClick: function () {
+        this.props.onGenreChange(this.props.link);
+    },
     render: function () {
-        return (<li>{this.props.data.genreName}</li>);
+        return (<li><a href="#" onClick={this.onClick}>{this.props.data.genreName}</a></li>);
     }
 });
 
